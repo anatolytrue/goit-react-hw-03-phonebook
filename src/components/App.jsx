c359df82beca71069cdbb6762c577cd348a20ec3
Anatolii <anatoly.tomnyak@gmail.com>
2022-09-27 15:33:24 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -15,21 +15,21 @@ class App extends Component {
   };
 
 
-  // componentDidMount() {
-  //   const contacts = localStorage.getItem('contacts')
-  //   const parsedContacts = JSON.parse(contacts)
-  //   if (parsedContacts) {
-  //     this.setState({contacts: parsedContacts})
-  //   }
-  // }
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts')
+    const parsedContacts = JSON.parse(contacts)
+    if (parsedContacts) {
+      this.setState({contacts: parsedContacts})
+    }
+  }
 
-  // componentDidUpdate(_, prevState) {
-  //   const nextContacts = this.state.contacts;
-  //   const prevContacts = prevState.contacts;
-  //   if (nextContacts !== prevContacts) {
-  //     localStorage.setItem('contacts', JSON.stringify(nextContacts))
-  //   }
-  // }
+  componentDidUpdate(prevProps, prevState) {
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+    if (nextContacts !== prevContacts) {
+      localStorage.setItem('contacts', JSON.stringify(nextContacts))
+    }
+  }
 
   changeFilter = e => {
         this.setState({ filter: e.currentTarget.value });

04b445c0de0354dcbed768bc4b0a8208d6e14810
Anatolii <anatoly.tomnyak@gmail.com>
2022-09-27 14:47:56 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -14,6 +14,23 @@ class App extends Component {
     filter: '',
   };
 
+
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts')
+    const parsedContacts = JSON.parse(contacts)
+    if (parsedContacts) {
+      this.setState({contacts: parsedContacts})
+    }
+  }
+
+  componentDidUpdate(_, prevState) {
+    const nextContacts = this.state.contacts;
+    const prevContacts = prevState.contacts;
+    if (nextContacts !== prevContacts) {
+      localStorage.setItem('contacts', JSON.stringify(nextContacts))
+    }
+  }
+
   changeFilter = e => {
         this.setState({ filter: e.currentTarget.value });
   }
